b461960685bcf832b7bfd70b4e11c3f453b23a26
Pogobanane <aenderboy@gmx.de>
2018-10-30 12:23:42 +0100

    move windows

--- main.c
+++ main.c
@@ -1,25 +1,34 @@
 #include <libwnck/libwnck.h>
 
-int
-main (int    argc,
-      char **argv)
+#define WNCK_WINDOW_CHANGE_EVERYTHING (WNCK_WINDOW_CHANGE_X | WNCK_WINDOW_CHANGE_Y | WNCK_WINDOW_CHANGE_WIDTH | WNCK_WINDOW_CHANGE_HEIGHT)
+
+int main (int argc, char **argv)
 {
   WnckScreen *screen;
   WnckWindow *active_window;
   GList *window_l;
 
-  gdk_init (&argc, &argv);
+  gdk_init(&argc, &argv);
 
-  screen = wnck_screen_get_default ();
+  screen = wnck_screen_get_default();
 
-  wnck_screen_force_update (screen);
+  wnck_screen_force_update(screen);
 
-  active_window = wnck_screen_get_active_window (screen);
+  active_window = wnck_screen_get_active_window(screen);
 
-  for (window_l = wnck_screen_get_windows (screen); window_l != NULL; window_l = window_l->next)
+  int xp, yp, widthp, heightp;
+  wnck_window_get_geometry(active_window, &xp, &yp, &widthp, &heightp);
+  g_print("%i, %i, %i, %i\n", xp, yp, widthp, heightp);
+
+  wnck_window_set_geometry(active_window, WNCK_WINDOW_GRAVITY_SOUTH, WNCK_WINDOW_CHANGE_EVERYTHING, xp, yp+50, widthp, heightp);
+  wnck_window_get_geometry(active_window, &xp, &yp, &widthp, &heightp);
+  g_print("%i, %i, %i, %i\n", xp, yp, widthp, heightp);
+
+
+  for (window_l = wnck_screen_get_windows(screen); window_l != NULL; window_l = window_l->next)
     {
-      WnckWindow *window = WNCK_WINDOW (window_l->data);
-      g_print ("%s%s\n", wnck_window_get_name (window),
+      WnckWindow *window = WNCK_WINDOW(window_l->data);
+      g_print ("%s%s\n", wnck_window_get_name(window),
                   window == active_window ? " (active)" : "");
     }
 
