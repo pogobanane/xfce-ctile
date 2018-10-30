84312d3f2bbe3ca9f65ddbf4eb1b5ecedca5d670
Pogobanane <aenderboy@gmx.de>
2018-10-30 12:28:39 +0100

    move move_window code to a function

--- main.c
+++ main.c
@@ -2,17 +2,9 @@
 
 #define WNCK_WINDOW_CHANGE_EVERYTHING (WNCK_WINDOW_CHANGE_X | WNCK_WINDOW_CHANGE_Y | WNCK_WINDOW_CHANGE_WIDTH | WNCK_WINDOW_CHANGE_HEIGHT)
 
-int main (int argc, char **argv)
-{
-  WnckScreen *screen;
+
+void move_active_window(WnckScreen* screen) {
   WnckWindow *active_window;
-  GList *window_l;
-
-  gdk_init(&argc, &argv);
-
-  screen = wnck_screen_get_default();
-
-  wnck_screen_force_update(screen);
 
   active_window = wnck_screen_get_active_window(screen);
 
@@ -24,12 +16,27 @@ int main (int argc, char **argv)
   wnck_window_get_geometry(active_window, &xp, &yp, &widthp, &heightp);
   g_print("%i, %i, %i, %i\n", xp, yp, widthp, heightp);
 
+}
+
+
+int main (int argc, char **argv)
+{
+  WnckScreen *screen;
+  GList *window_l;
+
+  gdk_init(&argc, &argv);
+
+  screen = wnck_screen_get_default();
+
+  wnck_screen_force_update(screen);
+
+  move_active_window(screen);
 
   for (window_l = wnck_screen_get_windows(screen); window_l != NULL; window_l = window_l->next)
     {
       WnckWindow *window = WNCK_WINDOW(window_l->data);
       g_print ("%s%s\n", wnck_window_get_name(window),
-                  window == active_window ? " (active)" : "");
+              /*window == active_window false ? " (active)" :*/ "");
     }
 
   return 0;
