b997b596d99faae70766571f52601b60072f4b28
Pogobanane <aenderboy@gmx.de>
2018-11-02 01:46:40 +0100

    testing main.c

--- main.c
+++ main.c
@@ -6,15 +6,28 @@
 
 void move_active_window(WnckScreen* screen) {
   WnckWindow *active_window;
+  GList *window_l;
 
   active_window = wnck_screen_get_active_window(screen);
+  g_print ("%s\n", wnck_window_get_name(active_window));
+
+
+  for (window_l = wnck_screen_get_windows(screen); window_l != NULL; window_l = window_l->next)
+    {
+      WnckWindow *window = WNCK_WINDOW(window_l->data);
+      g_print ("%s%s\n", wnck_window_get_name(window),
+              window == active_window ? " (active)" : "");
+    }
 
   int xp, yp, widthp, heightp;
   wnck_window_get_geometry(active_window, &xp, &yp, &widthp, &heightp);
   g_print("%i, %i, %i, %i\n", xp, yp, widthp, heightp);
 
   wnck_window_set_geometry(active_window, WNCK_WINDOW_GRAVITY_SOUTH, WNCK_WINDOW_CHANGE_EVERYTHING, xp, yp+50, widthp, heightp);
+  sleep(1);
   wnck_screen_force_update(screen);
+  active_window = wnck_screen_get_active_window(screen);
+  xp = 0; yp = 0; widthp = 0; heightp = 0;
   wnck_window_get_geometry(active_window, &xp, &yp, &widthp, &heightp);
   g_print("%i, %i, %i, %i\n", xp, yp, widthp, heightp);
 
@@ -24,7 +37,6 @@ void move_active_window(WnckScreen* screen) {
 int main (int argc, char **argv)
 {
   WnckScreen *screen;
-  GList *window_l;
 
   gdk_init(&argc, &argv);
 
@@ -37,12 +49,6 @@ int main (int argc, char **argv)
     xhandle_wait_event(handle);
     move_active_window(screen);
   }
-  for (window_l = wnck_screen_get_windows(screen); window_l != NULL; window_l = window_l->next)
-    {
-      WnckWindow *window = WNCK_WINDOW(window_l->data);
-      g_print ("%s%s\n", wnck_window_get_name(window),
-              /*window == active_window false ? " (active)" :*/ "");
-    }
   g_print("%s", "waiting for strg shift y");
 
   return 0;
