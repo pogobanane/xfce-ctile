68a1bea03fd907b8bcfb2702c98285b2616b0aba
Pogobanane <aenderboy@gmx.de>
2018-11-16 21:32:42 +0100

    add arrow key binds

--- src/keybinds.c
+++ src/keybinds.c
@@ -11,7 +11,7 @@ struct XHandle {
   Window root;
   XEvent ev;
   unsigned int modifiers;
-  int keycode;
+  int kc1, kc2, kc3, kc4; // keycodes
   Window grab_window;
   Bool owner_events;
   int pointer_mode;
@@ -23,25 +23,36 @@ struct XHandle xhandle_init_hotkeys() {
   handle.dpy = XOpenDisplay(0);
   handle.root = DefaultRootWindow(handle.dpy);
   handle.modifiers = ControlMask | ShiftMask;
-  handle.keycode = XKeysymToKeycode(handle.dpy,XK_Y); // strg shift y
+  handle.kc1 = XKeysymToKeycode(handle.dpy,XK_Left);
+  handle.kc2 = XKeysymToKeycode(handle.dpy,XK_Up);
+  handle.kc3 = XKeysymToKeycode(handle.dpy,XK_Right);
+  handle.kc4 = XKeysymToKeycode(handle.dpy,XK_Down);
   handle.grab_window = handle.root;
   handle.owner_events = False;
   handle.pointer_mode = GrabModeAsync;
   handle.keyboard_mode = GrabModeAsync;
-  XGrabKey(handle.dpy, handle.keycode, handle.modifiers, handle.grab_window, handle.owner_events, handle.pointer_mode,
+  XGrabKey(handle.dpy, handle.kc1, handle.modifiers, handle.grab_window, handle.owner_events, handle.pointer_mode,
            handle.keyboard_mode);
+  XGrabKey(handle.dpy, handle.kc2, handle.modifiers, handle.grab_window, handle.owner_events, handle.pointer_mode,
+          handle.keyboard_mode);
+  XGrabKey(handle.dpy, handle.kc3, handle.modifiers, handle.grab_window, handle.owner_events, handle.pointer_mode,
+           handle.keyboard_mode);
+  XGrabKey(handle.dpy, handle.kc4, handle.modifiers, handle.grab_window, handle.owner_events, handle.pointer_mode,
+          handle.keyboard_mode);
   XSelectInput(handle.dpy, handle.root, KeyPressMask );
   return handle;
 }
 
-void xhandle_wait_event(struct XHandle handle) {
+// returns a keycode. Note: XKeysymToKeycode(handle.dpy,XK_Left) with keysyms
+// from X11/keysymdef.h.
+unsigned int xhandle_wait_event(struct XHandle handle) {
   while (true) {
     XNextEvent(handle.dpy, &(handle.ev));
     switch(handle.ev.type)
     {
         case KeyPress:
-            printf("%s", "Hot key pressed!\n");
-            return;
+            printf("Hot key pressed! Keycode: %x\n", handle.ev.xkey.keycode);
+            return handle.ev.xkey.keycode;
         default:
             break;
     }
@@ -49,7 +60,10 @@ void xhandle_wait_event(struct XHandle handle) {
 }
 
 void xhandle_close(struct XHandle handle) {
-  XUngrabKey(handle.dpy,handle.keycode,handle.modifiers,handle.grab_window);
+  XUngrabKey(handle.dpy,handle.kc1,handle.modifiers,handle.grab_window);
+  XUngrabKey(handle.dpy,handle.kc2,handle.modifiers,handle.grab_window);
+  XUngrabKey(handle.dpy,handle.kc3,handle.modifiers,handle.grab_window);
+  XUngrabKey(handle.dpy,handle.kc4,handle.modifiers,handle.grab_window);
   XCloseDisplay(handle.dpy);
 }
 
